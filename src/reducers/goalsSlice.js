6c00dcf5a779df4f1cc7b57609a374c9c0fdec18
Javier Eduardo Lara Garcia <158225079+JavierProgramming@users.noreply.github.com>
2025-05-30 13:54:41 -0600

    goalsSlice.js

--- src/reducers/goalsSlice.js
+++ src/reducers/goalsSlice.js
@@ -1,24 +1,34 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-export const todoSlice = createSlice({
+export const goalsSlice = createSlice({
     name: 'goals',
     initialState: {
         value: [
             {
-                'name':'Graduarme de la Universidad',
-                'description':'Ganar todos los cursos',
-                'dueDate':'2028-04-20'
+                name: 'Aprender Inglés Avanzado',
+                description: 'Estudiar y practicar diariamente para obtener certificación C1',
+                dueDate: '2026-06-15'
+            },
+            {
+                name: 'Ahorrar para un auto',
+                description: 'Ahorrar mensualmente para comprar un auto usado en buen estado',
+                dueDate: '2027-12-01'
+            },
+            {
+                name: 'Crear un emprendimiento digital',
+                description: 'Lanzar una tienda en línea de productos sostenibles',
+                dueDate: '2026-03-30'
             }
         ],
     },
     reducers: {
         addGoal: (state, action) => {
-            state.value.push(action.payload)
+            state.value.push(action.payload);
         }
     }
-})
+});
 
-export const { addGoal } =  todoSlice.actions
-export const selectGoals = (state) => state.goals.value
+export const { addGoal } = goalsSlice.actions;
+export const selectGoals = (state) => state.goals.value;
 
-export default todoSlice.reducer
+export default goalsSlice.reducer;
