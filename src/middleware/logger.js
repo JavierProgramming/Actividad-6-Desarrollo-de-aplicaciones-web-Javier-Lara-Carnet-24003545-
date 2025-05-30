74ea47cde735aa58adcedbaa1a9b17ee0d103763
Javier Eduardo Lara Garcia <158225079+JavierProgramming@users.noreply.github.com>
2025-05-30 13:49:51 -0600

    logger.js

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,11 +1,10 @@
 const logger = (store) => (next) => (action) => {
-    console.group(action);
-    console.log("The action: ", action);
-    const result = next(action);
-    console.log("The new state: ", store.getState());
-    console.groupEnd();
-    return result;
-  };
-  
-  export default logger;
-  
+  console.group(action.type);
+  console.log("Dispatching action:", action);
+  const result = next(action);
+  console.log("Updated state:", store.getState());
+  console.groupEnd();
+  return result;
+};
+
+export default logger;
