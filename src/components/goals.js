9f1561d00cd941f6a4e5ee692cb5cf7138a1f4c1
Javier Eduardo Lara Garcia <158225079+JavierProgramming@users.noreply.github.com>
2025-05-30 12:24:15 -0600

    goals.js

--- src/components/goals.js
+++ src/components/goals.js
@@ -1,27 +1,36 @@
+import React, { useRef } from 'react';
 import List from "./list";
 import { useSelector, useDispatch } from 'react-redux';
-import { useEffect, useState} from 'react';
-import {
-    addGoal,
-    selectGoals
-} from '../reducers/goalsSlice'
-import { useRef } from "react";
-export function Goals(){
-    const dispatch = useDispatch();
-    const todos = useSelector(selectGoals);
-    const inputRef = useRef();
-    const addItem = (e) => {
-        e.preventDefault();
-        dispatch(addGoal({'name': inputRef.current.value}))
-    }
+import { addGoal, selectGoals } from '../reducers/goalsSlice';
 
-    return (
-        <div>
-          <h1>Goals List</h1>
-          <input type="text" placeholder="Add Todo" ref={inputRef} />
-          <button onClick={addItem}>Add Todo</button>
-    
-          <List items={todos} />
-        </div>
-      );
+export function Goals() {
+  const dispatch = useDispatch();
+  const goals = useSelector(selectGoals);
+  const inputRef = useRef();
+
+  const handleAddGoal = (e) => {
+    e.preventDefault();
+    const name = inputRef.current.value.trim();
+    if (name) {
+      dispatch(addGoal({ name }));
+      inputRef.current.value = '';
+    }
+  };
+
+  return (
+    <div className="goals-container">
+      <h1>Goals List</h1>
+      <form onSubmit={handleAddGoal} className="goals-form">
+        <input 
+          type="text" 
+          placeholder="Add Goal" 
+          ref={inputRef} 
+          aria-label="Goal name"
+          className="goal-input"
+        />
+        <button type="submit" className="btn btn-primary">Add Goal</button>
+      </form>
+      <List items={goals} />
+    </div>
+  );
 }
